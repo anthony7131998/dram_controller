e740865beb17c398b0faf988a42edbc0a6d32c3f
unknown <anthony7311998@gmail.com>
2022-03-24 16:21:27 -0400

    Reneamed module to addr_translator. Changed microarch to include decoders.

---
+++ rtl/dram_addr_translator.v
@@ -0,0 +1,31 @@
+// dram_decoder.v
+
+
+module dram_address_translator #(
+    parameter integer ADDR_WIDTH=13,
+    parameter integer NUM_OF_BANKS=8,
+    parameter integer NUM_OF_ROWS=128,
+    parameter integer NUM_OF_COLS=8
+) (
+    input [ADDR_WIDTH-1:0] l2_req_address,
+    output [$clog2(NUM_OF_BANKS)-1:0] bank_id,
+    output [$clog2(NUM_OF_ROWS)-1:0] row_id,
+    output [$clog2(NUM_OF_COLS)-1:0] col_id
+);
+
+    // Bank0: 0-3ff     (0 0000 0000 0000 - 0 0011 1111 1111) 
+    // Bank1: 400-7ff   (0 0100 0000 0000 - 0 0111 1111 1111)
+    // Bank2: 800-Bff   (0 1000 0000 0000 - 0 1011 1111 1111)
+    // Bank3: C00-fff   (0 1100 0000 0000 - 0 1111 1111 1111)
+    // Bank4: 1000-13ff (1 0000 0000 0000 - 1 0011 1111 1111)
+    // Bank5: 1400-17ff (1 0100 0000 0000 - 1 0111 1111 1111)
+    // Bank6: 1800-1bff (1 1000 0000 0000 - 1 1000 0000 0000)
+    // Bank7: 1c00-1fff (1 1100 0000 0000 - 1 1111 1111 1111)
+
+
+    // Overall DRAM Address Range: 0-1fff
+    assign bank_id = l2_req_address[ADDR_WIDTH-1:ADDR_WIDTH-3];
+    assign row_id  = l2_req_address[ADDR_WIDTH-4:ADDR_WIDTH-10];
+    assign col_id  = l2_req_address[2:0];
+
+endmodule
---
+++ tb/dram_addr_translator_tb.v
@@ -0,0 +1,62 @@
+
+`timescale 1ns/100ps
+
+module dram_address_translator_tb;
+
+    localparam NUMBER_OF_TESTS = 8192;
+    localparam ADDR_WIDTH = 13;
+    localparam NUM_OF_BANKS = 8;
+    localparam NUM_OF_ROWS = 128;
+    localparam NUM_OF_COLS = 8;
+
+    reg [ADDR_WIDTH-1:0] l2_req_address;
+    reg [$clog2(NUM_OF_BANKS)-1:0] bank_id;
+    reg [$clog2(NUM_OF_ROWS)-1:0] row_id;
+    reg [$clog2(NUM_OF_COLS)-1:0] col_id;
+
+    reg [$clog2(NUM_OF_BANKS)-1:0] correct_bank_id;
+    reg [$clog2(NUM_OF_ROWS)-1:0] correct_row_id;
+    reg [$clog2(NUM_OF_COLS)-1:0] correct_col_id;
+
+    integer total_errors=0;
+
+    dram_addr_translator #(
+        .ADDR_WIDTH   (13),
+        .NUM_OF_BANKS (8),
+        .NUM_OF_ROWS  (128),
+        .NUM_OF_COLS  (8)
+    ) dut (.*);
+
+    initial begin : drive_signals
+        integer i;
+        for(i=0; i<NUMBER_OF_TESTS; i=i+1) begin
+            #10 l2_req_address <= i;
+        end
+        $display("Finished Simulation. Total Errors %0d", total_errors);
+    end
+
+    initial begin : check_signals
+        integer i;
+        for(i=0; i<NUMBER_OF_TESTS; i=i+1) begin
+            correct_bank_id = i[12:10];
+            correct_row_id = i[9:3];
+            correct_col_id = i[2:0];
+
+            if(correct_bank_id != bank_id) begin
+                $display("Error (%0t) Incorrect BankID....... BankID %0h Correct %0h", $time, bank_id, correct_bank_id);
+                total_errors = total_errors + 1;
+            end
+
+            if(correct_row_id != row_id) begin
+                $display("Error (%0t) Incorrect RowID....... RowID %0h Correct %0h", $time, row_id, correct_row_id);
+                total_errors = total_errors + 1;
+            end
+
+            if(correct_col_id != col_id) begin
+                $display("Error (%0t) Incorrect ColID....... ColID %0h Correct %0h", $time, col_id, correct_col_id);
+                total_errors = total_errors + 1;
+            end
+        end
+    end
+
+endmodule
