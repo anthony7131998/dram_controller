13a6d2d4de66f0776e306fe72d0afcd42dc76ad1
Daniel <garantond@gmail.com>
2022-03-29 18:27:30 -0400

    add

--- tb/dram_bfm.v
+++ tb/dram_bfm.v
@@ -31,8 +31,16 @@ module dram_bfm #(
     reg data_out;
 
     reg [7:0] buffers [NUM_OF_BANKS-1:0];
+    reg [7:0] buffer_tmp0;
+    reg [7:0] buffer_tmp1;
+    reg [7:0] buffer_tmp2;
+    reg [7:0] buffer_tmp3;
+    reg [7:0] buffer_tmp4;
+    reg [7:0] buffer_tmp5;
+    reg [7:0] buffer_tmp6;
+    reg [7:0] buffer_tmp7;
 
-    assign data = (!bank_rw && !buffer_rw) ? data_out : 1'bz;
+    assign data = (!buffer_rw) ? data_out : 1'bz;
 
     always @(posedge clk or negedge rst_b) begin
         if(!rst_b) begin
@@ -48,6 +56,15 @@ module dram_bfm #(
                     bank7[i][j] <= '0;
 
                     buffers[j] <= '0;
+                    buffer_tmp0 <= '0;
+                    buffer_tmp1 <= '0;
+                    buffer_tmp2 <= '0;
+                    buffer_tmp3 <= '0;
+                    buffer_tmp4 <= '0;
+                    buffer_tmp5 <= '0;
+                    buffer_tmp6 <= '0;
+                    buffer_tmp7 <= '0;
+
                 end
             end
         end else begin
@@ -63,15 +80,25 @@ module dram_bfm #(
                     3'b111: bank7[rowid][colid] <= data;
                 endcase
             end else if(buffer_rw) begin
+                for(i=0; i<NUM_OF_COLS ; i=i+1) begin
+                    buffer_tmp0[i] <= bank0[rowid][i];
+                    buffer_tmp1[i] <= bank1[rowid][i];
+                    buffer_tmp2[i] <= bank2[rowid][i];
+                    buffer_tmp3[i] <= bank3[rowid][i];
+                    buffer_tmp4[i] <= bank4[rowid][i];
+                    buffer_tmp5[i] <= bank5[rowid][i];
+                    buffer_tmp6[i] <= bank6[rowid][i];
+                    buffer_tmp7[i] <= bank7[rowid][i];
+                end
                 case(bank_id)
-                    3'b000: buffers[0] <= bank0[rowid][colid];
-                    3'b001: buffers[1] <= bank1[rowid][colid];
-                    3'b010: buffers[2] <= bank2[rowid][colid];
-                    3'b011: buffers[3] <= bank3[rowid][colid];
-                    3'b100: buffers[4] <= bank4[rowid][colid];
-                    3'b101: buffers[5] <= bank5[rowid][colid];
-                    3'b110: buffers[6] <= bank6[rowid][colid];
-                    3'b111: buffers[7] <= bank7[rowid][colid];
+                        3'b000: buffers[0] <= buffer_tmp0;
+                        3'b001: buffers[1] <= buffer_tmp1;
+                        3'b010: buffers[2] <= buffer_tmp2;
+                        3'b011: buffers[3] <= buffer_tmp3;
+                        3'b100: buffers[4] <= buffer_tmp4;
+                        3'b101: buffers[5] <= buffer_tmp5;
+                        3'b110: buffers[6] <= buffer_tmp6;
+                        3'b111: buffers[7] <= buffer_tmp7;
                 endcase            
             end
         end
@@ -83,14 +110,14 @@ module dram_bfm #(
         end else begin
             if(!buffer_rw) begin
                 case(bank_id)
-                    3'b000: data_out <= buffers[0];
-                    3'b001: data_out <= buffers[1];
-                    3'b010: data_out <= buffers[2];
-                    3'b011: data_out <= buffers[3];
-                    3'b100: data_out <= buffers[4];
-                    3'b101: data_out <= buffers[5];
-                    3'b110: data_out <= buffers[6];
-                    3'b111: data_out <= buffers[7];
+                    3'b000: data_out <= buffers[0][colid];
+                    3'b001: data_out <= buffers[1][colid];
+                    3'b010: data_out <= buffers[2][colid];
+                    3'b011: data_out <= buffers[3][colid];
+                    3'b100: data_out <= buffers[4][colid];
+                    3'b101: data_out <= buffers[5][colid];
+                    3'b110: data_out <= buffers[6][colid];
+                    3'b111: data_out <= buffers[7][colid];
                 endcase
             end
         end
